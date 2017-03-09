4a4da7db578a4cc6a0fbbf46e721969ea562d8c7
johns224 <johns224@gmail.com>
2017-03-09 17:50:45 -0600

    finished abstraction to VeraController

--- src/main/java/org/rossjohnson/notification/controller/VeraNotificationController.java
+++ src/main/java/org/rossjohnson/notification/controller/VeraNotificationController.java
@@ -1,11 +1,16 @@
 package org.rossjohnson.notification.controller;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import org.rossjohnson.notification.vera.VeraController;
-import org.rossjohnson.notification.vera.ArcadeController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 
 @RestController
@@ -14,42 +19,44 @@ public class VeraNotificationController {
     @Value("${vera.ip}")
     private String veraIPAddress;
 
-    @Value("${arcade.deviceId}")
-    private String arcadeDeviceId;
+    @Value("${vera.device.names.ids}")
+    private String deviceMapFromConfig;
 
-    private VeraController arcadeController;
+    private Map<String, String> deviceNameToIdMap;
+    private static Logger log = LoggerFactory.getLogger(VeraNotificationController.class);
 
     @RequestMapping("/vera-toggle")
     public String index(@RequestParam(value = "device", defaultValue = "arcade") String device) {
 
-        if ("arcade".equals(device)) {
-            init();
-            arcadeController.togglePower();
-            return "Arcade is now " + (arcadeController.isPowerOn() ? "on" : "off");
-        }
-        return "";
+        init();
+        VeraController controller = getController(deviceNameToIdMap.get(device));
+        controller.togglePower();
+        return "Arcade is now " + (controller.isPowerOn() ? "on" : "off");
     }
 
     @RequestMapping("/vera-query")
     public String query(@RequestParam(value = "device", defaultValue = "arcade") String device) {
 
-        if ("arcade".equals(device)) {
-            init();
-            boolean arcadePowerOn = arcadeController.isPowerOn();
-            return String.format(
-                    "%s is %s<p/><form action=\"/vera-toggle\"><button type=\"submit\">Turn %s</button></form>",
-                    device,
-                    arcadePowerOn ? "on" : "off",
-                    arcadePowerOn ? "off" : "on"
-            );
-        }
-        return "";
+        init();
+        boolean arcadePowerOn = getController(deviceNameToIdMap.get(device)).isPowerOn();
+        return String.format(
+                "%s is %s<p/><form action=\"/vera-toggle\"><button type=\"submit\">Turn %s</button></form>",
+                device,
+                arcadePowerOn ? "on" : "off",
+                arcadePowerOn ? "off" : "on"
+        );
     }
 
     public void init() {
-        if (arcadeController == null) {
-            arcadeController = new ArcadeController(veraIPAddress, arcadeDeviceId);
+        if (deviceNameToIdMap == null) {
+            deviceNameToIdMap = Splitter.on(",").withKeyValueSeparator("=").split(deviceMapFromConfig);
+            log.info(String.format("Initialized Vera Device Map with device/IDs: %s",
+                    Joiner.on(", ").withKeyValueSeparator("->").join(deviceNameToIdMap)));
         }
+    }
+
+    public VeraController getController(String deviceId) {
+        return new VeraController(veraIPAddress, deviceId);
     }
 
 }
